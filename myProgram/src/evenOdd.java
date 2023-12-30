333108b9aca7e58d5372ef259ce0b46d3dc6a68c
fulara1102 <fularagaurav1102@gmail.com>
2023-12-30 11:52:23 +0530

    COde added

---
+++ myProgram/src/evenOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class evenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        int num = in.nextInt();
+
+
+        if(num %2 == 0){
+            System.out.println("The number " +num+ " is even");
+
+        }
+        else {
+            System.out.println("The number "+num+" is Odd");
+        }
+
+    }
+}
